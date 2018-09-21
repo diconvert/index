2190a1d225b77a65d8a0ea83d30a478efd7f3a25
diconvert <43456020+diconvert@users.noreply.github.com>
2018-09-22 05:15:19 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="reasons-you-need-to-have-adequate-car.html"}
 if(x=="2"){xxx="how-to-choose-best-money-transfer.html"}
 if(x=="3"){xxx="how-can-diabetes-affect-your-eyes.html"}
-if(x=="4"){xxx="what-are-benefits-of-financial.html"}a_to_ve[i].href="https://www.diconvert.com/2018/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="what-are-benefits-of-financial.html"}a_to_ve[i].href="http://www.diconvert.com/2018/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
